596cd612679f74bab468216634e02a35d50ae5be
Vincent Ceulemans <vincent.ceulemans.120@gmail.com>
2014-12-10 23:26:27 +0100

    remove trailing whitespace

--- js/Mouse.js
+++ js/Mouse.js
@@ -1,8 +1,8 @@
-/**  
+/**
  * @module Presenter
  */
- var Presenter = Presenter || {};
- 
+var Presenter = Presenter || {};
+
 (function(global) {
     "use strict";
     /**
@@ -12,7 +12,7 @@
     function Mouse(){
        this.enable();
     }
-    
+
     Mouse.prototype.enable = function()
     {
         console.log("[Mouse] Enabled");
@@ -24,39 +24,39 @@
                     Mouse.scrollHandler(e.wheelDelta);
                 },
             false);
-            
+
             // Firefox
-            // Scroll information is stored in e.detail. Is '3' for scrollDown and '-3' for scrollUp. 
+            // Scroll information is stored in e.detail. Is '3' for scrollDown and '-3' for scrollUp.
             document.querySelector(".slideDeck").addEventListener("DOMMouseScroll",
                 function(e){
-                    Mouse.scrollHandler(-e.detail);  
+                    Mouse.scrollHandler(-e.detail);
                 },
             false);
         }
     };
 
     /**
-     * @param wheelData Negative number on scrollUp, positive number on scrollDown. 
-     */ 
+     * @param wheelData Negative number on scrollUp, positive number on scrollDown.
+     */
     Mouse.scrollHandler = function(wheelDelta){
-        var action = ""; 
+        var action = "";
         if(wheelDelta < 0){
-            action = "next"; 
+            action = "next";
         }
         else{
             action = "previous"
         }
-        
+
         global.postal.channel("slides").publish("navigator", {action: action});
     }
 
     Mouse.prototype.disable = function()
     {
-        console.log("[Mouse] Disabled"); 
+        console.log("[Mouse] Disabled");
         document.querySelector(".slideDeck").removeEventListener("mousewheel", Mouse.scrollHandler);
         document.querySelector(".slideDeck").removeEventListener("DOMMouseScroll",  Mouse.scrollHandler);
     };
 
-    //Make constructor visible in global space. 
-    global.Presenter.Mouse = Mouse; 
+    //Make constructor visible in global space.
+    global.Presenter.Mouse = Mouse;
 }(window));
