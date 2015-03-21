2f429625da646fbdc60df0632f664c07edbb7cbc
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2015-03-21 15:11:53 +0100

    Add handling for missing console methods

--- js/SlideDeckManager.js
+++ js/SlideDeckManager.js
@@ -1,8 +1,8 @@
-/**  
+/**
  * @module Presenter
  */
  var Presenter = Presenter || {};
- 
+
 (function(namespace, window) {
     "use strict";
     var PREFIX = Presenter.PREFIX = ["-webkit-", "-moz-", "-o-", ""];
@@ -10,14 +10,20 @@
     var SLIDE_TRANSFORMS = {};
 
     /**
-     * Starts the presentation. 
-     * 
+     * Starts the presentation.
+     *
      * @method init
      * @param {object} config Configuration object
      */
     function init(config) {
         Presenter.settings = config;
-        
+
+        //Redefine the log functions of console if the console object doesn't exist (ex. IE)
+        if(typeof console == "undefined"){
+            var console = {};
+            console.log = console.error = console.info = console.debug = function(){};
+        }
+
         //Hook up eventhandlers.
         $(window).on("resize", Presenter.onWindowResized);
 
@@ -25,23 +31,23 @@
             Presenter.Touch.enable();
         }
         Presenter.keyboard = new Presenter.Keyboard();
-        Presenter.mouse = new Presenter.Mouse(); 
-            
-        addExtraHTMLElements(); 
-        createDeck(); 
+        Presenter.mouse = new Presenter.Mouse();
+
+        addExtraHTMLElements();
+        createDeck();
         Presenter.onWindowResized();
-       
+
         //Overview
         if(typeof Presenter.Overview != 'undefined'){
-            Presenter.overview = new Presenter.Overview(deck);        
+            Presenter.overview = new Presenter.Overview(deck);
         }
-        
-        Presenter.Navigator.init(); 
+
+        Presenter.Navigator.init();
         window.postal.channel("slides").publish("slide-changed");
     }
 
     /**
-     * Adds needed HTML markup to the DOM. 
+     * Adds needed HTML markup to the DOM.
      *
      *  @method addExtraHTMLElements
      */
@@ -60,10 +66,10 @@
                         )
                 );
 
-        //Add the "slide" class to each section. 
+        //Add the "slide" class to each section.
         $(".slideDeck > section").addClass("slide");
     }
-    
+
     /**
      * Create an initialize a new deck object
      *
@@ -80,9 +86,9 @@
             var slideNumber = location.hash.substring(1); //location.hash is of the form "#number". Start from position 1 so the "#" is ignored.
             currentSlide = parseInt(slideNumber);
         }
-        
+
         deck = Presenter.deck = new Presenter.SlideDeck(document.querySelectorAll(".slide"), currentSlide);
-        var computedStyle = document.defaultView.getComputedStyle(deck.slides[0], ""); 
+        var computedStyle = document.defaultView.getComputedStyle(deck.slides[0], "");
         deck.slideWidth = parseInt(computedStyle.getPropertyValue("width"));
         deck.slideHeight = parseInt(computedStyle.getPropertyValue("height"));
 
@@ -91,33 +97,33 @@
             if (t > 0 && t <= deck.nSlides) {
                 deck.getSlide(t).classList.add(Presenter.SlideDeck.SLIDE_STATES[i]);
             }
-            
+
             //Store Original Transformation
-            //Add dummy slide to DOM to get the style associated. 
+            //Add dummy slide to DOM to get the style associated.
             $("body").append(
                 $("<div/>")
                         .attr("class", Presenter.SlideDeck.SLIDE_STATES[i])
                         .attr("id", "test")
-            );             
-            
-            var slide_style; 
+            );
+
+            var slide_style;
             if( $("#test").css('transform')){
-                slide_style =  $("#test").css('transform'); 
+                slide_style =  $("#test").css('transform');
             }
             else{
-                slide_style = $("#test").css('-webkit-transform'); 
+                slide_style = $("#test").css('-webkit-transform');
             }
 
             SLIDE_TRANSFORMS[Presenter.SlideDeck.SLIDE_STATES[i]] = slide_style;
 
-            //Remove Dummy. 
-            $("#test").remove(); 
+            //Remove Dummy.
+            $("#test").remove();
         }
     }
-    
+
     /**
-     * Resizes slidedeck when window is resized. 
-     * 
+     * Resizes slidedeck when window is resized.
+     *
      * @method windowResizedHandler
      */
     Presenter.onWindowResized = function(){
@@ -125,12 +131,12 @@
         var scale = Math.min(window.innerHeight / deck.slideHeight, window.innerWidth / deck.slideWidth);
 
         if(Presenter.settings.hasOwnProperty("scale")){
-            scale *= Presenter.settings.scale; 
+            scale *= Presenter.settings.scale;
         }
-        
+
         var offsetLeft = (window.innerWidth - deck.slideWidth * scale) / 2.0;
         var offsetTop = (window.innerHeight - deck.slideHeight * scale) / 2.0;
-        
+
         for (var i = 0; i < Presenter.SlideDeck.SLIDE_STATES.length; i++) {
             styles += "." + Presenter.SlideDeck.SLIDE_STATES[i] + "{\n";
             for (var j = 0; j < PREFIX.length; j++) {
@@ -141,13 +147,13 @@
             styles += "top: " + offsetTop + "px;\n";
             styles += "}\n";
         }
-        styles+="\n"; 
-        $("#style").html(styles); 
+        styles+="\n";
+        $("#style").html(styles);
     }
 
     Presenter.disableTransitions = function(){
         var styles = "";
-        
+
         for (var i = 0; i < Presenter.SlideDeck.SLIDE_STATES.length; i++) {
             styles += "." + Presenter.SlideDeck.SLIDE_STATES[i] + "{\n";
             for (var j = 0; j < PREFIX.length; j++) {
@@ -156,8 +162,8 @@
             }
             styles += "}\n";
         }
-        $("#style").html(styles); 
+        $("#style").html(styles);
     }
-    
+
     window.Presenter.init = init;
 }(Presenter, window));
